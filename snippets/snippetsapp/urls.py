b2dba1debd619dc87cda0d582773998a18255d13
Bill <keepgoingbill@gmail.com>
2023-05-17 22:57:02 +0800

    体会图形界面的出现，Response功能实现的

--- snippets/snippetsapp/urls.py
+++ snippets/snippetsapp/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path # 与bili课不同，因版本差异，bili课是1.10版本
+# from django.urls import path # 与bili课不同，因版本差异，bili课是1.10版本
+# from snippetsapp import views
+#
+# urlpatterns = [
+#     path('snippets/', views.snippet_list),
+#     path('snippets/<int:pk>/', views.snippet_detail), #此处也是和bili课差异较大，尤其int：pk那里
+# ]
+
+
+
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippetsapp import views
+
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail), #此处也是和bili课差异较大，尤其int：pk那里
+    path('snippets/<int:pk>/', views.snippet_list),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
