0d28a7a3d9667fc52731af93cac9a719a124c38b
Bill <keepgoingbill@gmail.com>
2023-05-31 15:36:12 +0800

    Create ProductByCategoryManufacturerView, ProductRetrieve

--- eshop/drf_shop-master/1/snippets/snippets/snippetsapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SnippetsappConfig(AppConfig):
-    name = 'snippetsapp'
--- eshop/drf_shop-master/5/eshop/eshop/eshop/computerapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComputerappConfig(AppConfig):
-    name = 'computerapp'
